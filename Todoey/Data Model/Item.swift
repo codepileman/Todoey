d5efdabe2094af7a7593c461851253a9a2a6e5ee
user147108 <user147108@74-80-245-5.ic2net.net>
2019-01-21 18:20:56 -0800

    Convert to MVC pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by user147108 on 1/20/19.
+//  Copyright © 2019 Chris Xu. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
